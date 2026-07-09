0f1e9e3fb665226e85a65338c5410ccff99cd91a
24x7work <work.24x7@hotmail.com>
2026-07-09 20:09:59 +0530

    Fix supabase client initialization syntax
    Add

--- js/config.js
+++ js/config.js
@@ -2,7 +2,8 @@ const SUPABASE_URL = "https://aapjrtvhjdxrkofioayj.supabase.co";
 
 const SUPABASE_ANON_KEY = "sb_publishable_22Nadm7464t_duOyHqiaYA_rQ0YcFXw";
 
-const supabase = window.supabase.createClient(
+// IMPORTANT
+window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
